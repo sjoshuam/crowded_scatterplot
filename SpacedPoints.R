8cbff807f1cf8bbb9522732eacc169d3ecac90c3
Josh M <70724269+sjoshuam@users.noreply.github.com>
2020-10-13 14:20:12 -0400

    Begin developing SpacedPoints() - an alternative approach to LumpedPoints()

--- SpacedPoints.R
+++ SpacedPoints.R
@@ -1,1 +1,40 @@
-##########
+##########==========##########==========##########==========##########==========
+
+##########========== HEADER
+
+## Meta-information
+  ## Author: Joshua Mendelsohn
+  ## Creation: 2020-10-09
+  ## Description: This script holds functions for rendering crowded scatterplots
+    ## using a point-spacing strategy. The demonstration before/after
+    ## scatterplot saves to "C_Outputs"
+
+## Environment set-up
+remove(list= objects())
+options(digits= 6, scipen= 4, width= 80, startTime= Sys.time())
+
+##== Inputs
+
+## DW-nominate scores for the 116th US Congress
+nominate_scores <- read.csv( file.path("A_Inputs", "HS116_members.csv"))
+
+##########========== REFINE TEST DATA
+
+## Strip out meta-data
+nominate_scores$Meta <- NULL
+
+## Simplify party codes
+match_index <- c( "R"= 200, "D"= 100, "I"= 328 )
+match_index <- match_index[match(nominate_scores$party_code, match_index)]
+nominate_scores$party_code <- names(match_index)
+remove(match_index)
+
+## Rename nominate coordinates (for convenience)
+nominate_scores$X <- nominate_scores$nominate_dim1
+nominate_scores$Y <- nominate_scores$nominate_dim2
+
+##########========== CREATE FUNCTIONS
+
+##########========== 
+
+##########==========##########==========##########==========##########==========
